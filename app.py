e17bde8e318bedea4e75187ea2b33c570b22e037
YaroslavIC <yaroslav.voroshilov@gmail.com>
2022-11-26 15:58:05 +0700

    tune app.py

--- app.py
+++ app.py
@@ -10,14 +10,14 @@ path_to_model = r'C:/bauman/git_bauman/ebw_model'
 
 minmax = pd.read_csv(path_to_model + r'/minmax.csv')
 
-print("¬ведите параметр IW (сварочный ток), значение между [",minmax.minIW,',',minmax.maxIW,']')
+print("¬ведите параметр IW (сварочный ток), значение между [%3.1f,%3.1f]" % (minmax.minIW,minmax.maxIW))
 IW_human_input = input()
 if IW_human_input=='':
     IW_human_input = (minmax.minIW+minmax.maxIW)/2
 else:
     IW_human_input = float(IW_human_input)
     
-print("¬ведите параметр IF (ток фокусировки эл. пучка), значение между [",minmax.minIF,',',minmax.maxIF,']')
+print("¬ведите параметр IF (ток фокусировки эл. пучка), значение между [%3.1f,%3.1f]" % (minmax.minIF,minmax.maxIF))
 IF_human_input = input()
 if IF_human_input=='':
     IF_human_input = (minmax.minIF+minmax.maxIF)/2
@@ -25,14 +25,14 @@ else:
     IF_human_input = float(IF_human_input)
 
 
-print("¬ведите параметр IF (ток фокусировки эл. пучка), значение между [",minmax.minVW,',',minmax.maxVW,']')
+print("¬ведите параметр VW (скорость сварки), значение между [%3.1f,%3.1f]" % (minmax.minVW,minmax.maxVW))
 VW_human_input = input()
 if VW_human_input=='':
     VW_human_input = (minmax.minVW+minmax.maxVW)/2
 else:
     VW_human_input = float(VW_human_input)
 
-print("¬ведите параметр IF (ток фокусировки эл. пучка), значение между [",minmax.minVW,',',minmax.maxVW,']')
+print("¬ведите параметр FP (рассто€ние до образца), значение между  [%3.1f,%3.1f]" % (minmax.minFP,minmax.maxFP))
 FP_human_input = input()
 if FP_human_input=='':
     FP_human_input = (minmax.minFP+minmax.maxFP)/2
@@ -50,22 +50,19 @@ human_input['IF'] = (human_input['IF'] - minmax.minIF) / (minmax.maxIF - minmax.
 human_input['VW'] = (human_input['VW'] - minmax.minVW) / (minmax.maxVW - minmax.minVW)
 human_input['FP'] = (human_input['FP'] - minmax.minFP) / (minmax.maxFP - minmax.minFP)
 
-#human_input = pd.DataFrame({'IW': [0.5],
-#                            'IF': [0.5],
-#                            'VW': [0.5],
-#                            'FP': [0.5]})
-
-
 model= keras.models.load_model(path_to_model)
 
 mypredcit = model.predict(human_input)
+Depth = mypredcit[:,0]*(minmax.maxDepth-minmax.minDepth)+minmax.minDepth
+Width = mypredcit[:,1]*(minmax.maxWidth-minmax.minWidth)+minmax.minWidth
 
-predict = pd.DataFrame({'Depth': mypredcit[:,0]*(minmax.maxDepth-minmax.minDepth)+minmax.minDepth, 
-                        'Width': mypredcit[:,1]*(minmax.maxWidth-minmax.minWidth)+minmax.minWidth})
+predict = pd.DataFrame({'Depth': Depth, 
+                        'Width': Width})
 
-# тестовые значение дл€ ответа: 0.92	1.86
- 
-print(predict)
+print("F(IW,IF,Vw,FP) = (Depth, Width)")
+print("F(%3.1f,%3.1f,%3.1f,%3.1f) = (%3.4f,%3.4f)"%(IW_human_input,IF_human_input,VW_human_input,FP_human_input,Depth,Width))
 
-#print("Predicted Depth: ",predict.Depth)
-#print("Predicted Width: ",predict.Width)
+
+
+#print(predict)
+
